2106e8be7eb7c0520cb2e5c61356b0a63f146e95
ParkerH27 <91337218+ParkerH27@users.noreply.github.com>
2023-04-24 14:06:56 +0000

    made into how many days left

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -7,22 +9,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
-});
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
-// Last Day for Students
-//https://calendar.google.com/calendar/ical/psdr3.org_3137383238353432373930%40resource.calendar.google.com/public/basic.ics
+var unixTimeMs = 0
+var events = null
+var isoString = null
+lastDay()
 
 
-// Sample link to an iCal file
-const icalUrl = 'https://calendar.google.com/calendar/ical/psdr3.org_auvngjhbljjajucngr199vdrbs%40group.calendar.google.com/public/basic.ics';
-const ical = require('ical.js');
+
 
 
 function findISOTimes(inputString) {
@@ -52,6 +46,16 @@ function findClosestFutureTime(timeList) {
 }
 
 
+
+
+function lastDay(){
+	const icalUrl = 'https://calendar.google.com/calendar/ical/psdr3.org_auvngjhbljjajucngr199vdrbs%40group.calendar.google.com/public/basic.ics';
+const ical = require('ical.js');
+
+
+
+
+
 // Load the iCal file
 fetch(icalUrl)
   .then(response => response.text())
@@ -60,7 +64,7 @@ fetch(icalUrl)
     const jcalData = ical.parse(data);
     const vcalendar = new ical.Component(jcalData);
     // Get all the events from the iCal file
-    const events = vcalendar.getAllSubcomponents('vevent');
+    events = vcalendar.getAllSubcomponents('vevent');
     // Filter events based on summary containing "Last Day for Students"
     const filteredEvents = events.filter(event => {
       const summary = event.getFirstPropertyValue('summary');
@@ -81,12 +85,8 @@ fetch(icalUrl)
 
 		// console.log(findIsoTimes(String(filteredEvents)))
 
-console.log("")
 inputStr = JSON.stringify(filteredEvents2)
-
-const regex = /(A|B|C|X)\s+Day|MOD\s+(A|B|C|X)/;
-const match = inputStr.match(regex);
-const letter = match ? (match[1] || match[2]) : null;
+var letter = findDayType(inputStr)
 
 // Set the time based on the letter
 let hour, minute;
@@ -99,14 +99,96 @@ if (letter === 'C') {
 }
 
 // Parse the date string and set the time
-const dateString = "2023-05-26";
+const dateString = String(date.split('T')[0]);
 const newdate = new Date(`${dateString}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00`);
 
 // Convert the date to ISO format and UNIX time
-const isoString = newdate.toISOString();
-const unixTimeMs = newdate.getTime(); // returns UNIX time in milliseconds
+isoString = newdate.toISOString();
+unixTimeMs = newdate.getTime(); // returns UNIX time in milliseconds
+		// output: 1685068740000 (or similar, depending on your timezone)
+		console.log(unixTimeMs)
+		return unixTimeMs
 
-console.log(isoString); // output: "2023-05-26T13:19:00.000Z"
-console.log(unixTimeMs); // output: 1685068740000 (or similar, depending on your timezone)
+  });}
 
+
+
+
+
+
+
+
+function createListOfDays(futureDateStr) {
+  const today = new Date();
+  const futureDate = new Date(futureDateStr);
+  const days = [];
+  
+  for (let date = today; date <= futureDate; date.setDate(date.getDate() + 1)) {
+    const formattedDate = date.toISOString().slice(0, 10);
+    days.push(formattedDate);
+  }
+  
+  return days;
+}
+
+function findDayType(inputStr){
+	const regex = /(A|B|C|X)\s+day|MOD\s+(A|B|C|X)/i;
+	const match = inputStr.match(regex);
+	let letter = match ? (match[1] || match[2]) : null;
+	return letter
+}
+function dayTypes(){
+	list = createListOfDays(isoString)
+	i = 1
+	console.log(list[i])
+	const filteredEvents3 = events.filter(event => {
+		  const dtStart = event.getFirstPropertyValue('dtstart').toString();
+		  return dtStart.includes(list[i].split('T')[0]);
+		
+    });
+
+	console.log(JSON.stringify(filteredEvents3))
+	console.log(findDayType(JSON.stringify(filteredEvents3)))
+	
+}
+
+function weekDays(days) {
+  // Initialize the count of working days.
+  let count = 0;
+
+  // Iterate over the list of days.
+  for (const day of days) {
+    // Convert the day string to a Date object.
+    const date = new Date(day);
+
+    // Get the day of the week from the Date object.
+    const dayOfWeek = date.getDay();
+
+    // If the day is not Saturday or Sunday, increment the count.
+    if (dayOfWeek !== 6 && dayOfWeek !== 0) {
+      count++;
+    }
+  }
+
+  // Return the count of working days.
+  return count-1;
+}
+
+
+
+
+
+
+io.on('connection', (socket) => {
+  socket.on('needLD', msg => {
+    io.emit('eoty', weekDays(createListOfDays(isoString)));
   });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+// Last Day for Students
+//https://calendar.google.com/calendar/ical/psdr3.org_3137383238353432373930%40resource.calendar.google.com/public/basic.ics
+
+
